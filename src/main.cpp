8fe4aa7ebb7f3fe4540e47cb2f76a6aa64e6ed18
wngkyle <99611120+wngkyle@users.noreply.github.com>
2023-11-03 11:10:38 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#define built_in_LED 13
+#define start_calibration_LED 41
+#define end_calibration_LED 40
+
+void setup_feedback();
+
+void setup() {
+  pinMode(built_in_LED, OUTPUT);
+  pinMode(start_calibration_LED, OUTPUT);
+  pinMode(end_calibration_LED, OUTPUT);
+
+}
+
+void loop() {
+ 
+}
+
+void setup_feedback() {
+  digitalWrite(built_in_LED, HIGH);
+  digitalWrite(start_calibration_LED, HIGH);
+
+  delay(4000);
+
+  digitalWrite(start_calibration_LED, LOW);
+  digitalWrite(end_calibration_LED, HIGH);
+
+}
